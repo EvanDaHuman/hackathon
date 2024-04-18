aa442901daa6e96b058073811fe1cf76a307cb81
Evan Wang <evandahuman@gmail.com>
2024-04-18 13:45:00 -0400

    h

--- src/App.js
+++ src/App.js
@@ -10,24 +10,30 @@ import Game4 from "./pages/game4";
 import Game5 from "./pages/game5";
 import WinPage from "./pages/win";
 import LosePage from "./pages/lose";
-import Frame from "./pages/figma1";
+
+const [totalPoints, settotalPoints] = useState(0);
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/stats" element={<Stats />} />
-        <Route path="/game/game1" element={<MovieDropDown />} />
-        <Route path="/game2" element={<Game2 />} />
-        <Route path="/game3" element={<Game3 />} />
-        <Route path="/game4" element={<Game4 />} />
-        <Route path="/game5" element={<Game5 />} />
-        <Route path="/win" element={<WinPage />} />
-        <Route path="/lose" element={<LosePage />} />
-      </Routes>
-    </Router>
+    <>
+    <div className = "point-display">
+      
+    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path = "/stats" element = {<Stats />} />
+          <Route path = "/game/game1" element = {<MovieDropDown />} />
+          <Route path = "/game2" element = {<Game2 />} />
+          <Route path = "/game3" element = {<Game3 />} />
+          <Route path = "/game4" element = {<Game4 />} />
+          <Route path = "/game5" element = {<Game5 />} />
+          <Route path = "/win" element = {<WinPage/>} />
+          <Route path = "/lose" element = {<LosePage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
